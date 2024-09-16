aa7bec7035fa40c37e5539a3a94a55012275ee08
Alcides Benitez <alcides_benitez@hacienda.gov.py>
2024-09-16 13:25:51 -0400

    Commit inicial

---
+++ src/main/java/com/marithe/sysfact/SysfactApplication.java
@@ -0,0 +1,13 @@
+package com.marithe.sysfact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysfactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysfactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marithe/sysfact/SysfactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marithe.sysfact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysfactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
